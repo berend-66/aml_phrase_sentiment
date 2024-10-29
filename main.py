41113d946a2808c2fa3eebcf76cb2ad073ad1940
jet.semrick@gmail.com <jet.semrick@gmail.com>
2024-10-29 16:30:40 -0400

    TF-IDF

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from sklearn.linear_model import LogisticRegression
 from preprocessing.PrincipalComponentAnalysis import PrincipalComponentAnalysis
 from preprocessing.PreProcess import PreProcess
 from preprocessing.BagOfWords import BagOfWords
+from preprocessing.TFIDF import TFIDF
 
 train_data = pd.read_csv('data/train.csv')
 val_data = pd.read_csv('data/val.csv')
@@ -64,12 +65,16 @@ combined_data = pd.concat([X_train_preprocess, X_val_preprocess, X_test_preproce
 bag = BagOfWords(combined_data)
 
 n = 10
-X_train = bag.bag_of_words(X_train_preprocess, threshold_m=n)
-X_train_clean = bag.bag_of_words(X_train_clean_preprocess, threshold_m=n)
-X_val = bag.bag_of_words(X_val_preprocess, threshold_m=n)
-X_test = bag.bag_of_words(X_test_preprocess, threshold_m=n)
+X_train_bag = bag.bag_of_words(X_train_preprocess, threshold_m=n)
+X_train_clean_bag = bag.bag_of_words(X_train_clean_preprocess, threshold_m=n)
+X_val_bag = bag.bag_of_words(X_val_preprocess, threshold_m=n)
+X_test_bag = bag.bag_of_words(X_test_preprocess, threshold_m=n)
+
+# TF-IDF
+tfidf = TFIDF()
+X_train_clean_tfidf = tfidf.fit(X_train_clean_preprocess)
 
 # Principle Component Analysis
 pca = PrincipalComponentAnalysis(10)
-X_pca = pca.fit(X_train_clean)
-pca.elbow_graph()
+X_train_clean_pca = pca.fit(X_train_clean_bag)
+# pca.elbow_graph()
---
+++ preprocessing/TFIDF.py
@@ -0,0 +1,16 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+class TFIDF:
+  def fit(self, data):
+    # Initialize the TF-IDF vectorizer with tunable parameters ---> Tune on validation set using grid search
+    tfidf_vectorizer = TfidfVectorizer(
+      max_features=1000,            # Limit to the top 1000 features
+      ngram_range=(1, 2),           # Include unigrams and bigrams
+      min_df=3,                     # Ignore terms that appear in fewer than 3 documents
+      max_df=0.9,                   # Ignore terms that appear in more than 90% of the documents
+      stop_words='english',         # Use English stopwords
+      sublinear_tf=True,            # Apply sublinear scaling to term frequency
+      use_idf=True                  # Use inverse document frequency
+    )
+
+    return tfidf_vectorizer.fit_transform(data)
