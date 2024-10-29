29b82928bb1131a1a846e78cd724c1e71c7db003
jet.semrick@gmail.com <jet.semrick@gmail.com>
2024-10-29 12:28:46 -0400

    data organization

--- main.py
+++ main.py
@@ -28,7 +28,27 @@ from sklearn.linear_model import LogisticRegression
 
 # import classes
 
-
 train_data = pd.read_csv('data/train.csv')
 val_data = pd.read_csv('data/val.csv')
 test_data = pd.read_csv('data/test.csv')
+
+# Training Data
+X_train = train_data['Phrase']
+y_train = train_data['Sentiment']
+
+mask = (y_train != -100) # filter out unlabeled data
+train_data_clean = train_data[mask]
+X_train_clean = X_train[mask]
+y_train_clean = y_train[mask]
+
+# Validation Data
+X_val = val_data['Phrase']
+y_val = val_data['Sentiment']
+
+# Test Data
+X_test = test_data['Phrase']
+
+print(f"Train Data Shape: {X_train.shape}")
+print(f"Cleaned Train Data Shape: {train_data_clean['Phrase'].shape}")
+print(f"Validation Data Shape: {X_val.shape}")
+print(f"Test Data Shape: {X_test.shape}")
